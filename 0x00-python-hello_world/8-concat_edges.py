d0100c415ca047d36b3f89e1e092cfd91054204d
rahma korish <rahmakorish@gmail.com>
2023-11-27 06:47:46 -0800

    8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,3 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(str.split()[5], str.split()[6], str.split()[12], str.split()[0])
+# end of code here
