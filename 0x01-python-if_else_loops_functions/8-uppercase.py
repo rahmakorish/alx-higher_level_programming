71bae77a9086f5b97055301307d615f9fae51933
rahma korish <rahmakorish@gmail.com>
2023-11-28 01:27:42 -0800

    8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in  range (len(str)):
+        x = ord(c)
+        if x < 90 and x > 64:
+            print(x)
+        else:
+            print(x)
