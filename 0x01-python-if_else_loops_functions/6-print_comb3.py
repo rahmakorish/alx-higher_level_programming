52e08b4d6627482b28532bf2aef78a8238becb65
rahma korish <rahmakorish@gmail.com>
2023-11-28 06:02:16 -0800

    6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for x in range(0, 10):
-        if(x == i):
+        if (x == i):
             pass
         elif int(f"{i:d}{x:d}") > int(f"{x:d}{i:d}"):
             pass
