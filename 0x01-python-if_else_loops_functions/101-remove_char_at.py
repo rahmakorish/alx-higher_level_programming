3935ae6bafe0ab6aa4112e3013953f995d1aef7e
rahma korish <rahmakorish@gmail.com>
2023-11-28 06:38:15 -0800

    1010

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    newstr = ""
+    for i in range(0, len(str)):
+        if i == n:
+            pass
+        else:
+            newstr += str[i]
+    return(newstr)
