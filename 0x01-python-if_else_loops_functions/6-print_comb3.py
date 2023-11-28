043d1eac02c01717f9ed6f48e48d0953744434f9
rahma korish <rahmakorish@gmail.com>
2023-11-28 06:01:07 -0800

    6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -6,6 +6,6 @@ for i in range(0, 10):
         elif int(f"{i:d}{x:d}") > int(f"{x:d}{i:d}"):
             pass
         elif int(f"{i:d}{x:d}") == 89:
-            print(f"{i:d}{x:d}", end="\n")
+            print(f"{i:d}{x:d}".format(i).format(x), end="\n")
         else:
-            print(f"{i:d}{x:d}, ", end="")
+            print(f"{i:d}{x:d}, ".format(i).format(x), end="")
