3c48fb7651229d537370085e0b16dcddc7750579
rahma korish <rahmakorish@gmail.com>
2023-11-30 12:12:53 -0800

    4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-import hidden_4 
+import hidden_4
 if __name__ == "__main__":
-    print(name)
+    for item in range(0, len(dir(hidden_4))):
+        if dir(hidden_4)[item].startswith('_'):
+            pass
+        else:
+            print(dir(hidden_4)[item])
