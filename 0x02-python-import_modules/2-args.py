db43508fcccd826f0cf4c0202c9268bf8769b1a9
rahma korish <rahmakorish@gmail.com>
2023-11-30 07:40:32 -0800

    fix 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import sys
-count = len((sys.argv)) - 1
-if count == 0:
-    print(f"{count:d} arguments.".format(count))
-elif count == 1:
-    print(f"{count:d} argument:".format(count))
-    for i in range(1, len(sys.argv)):
-        print(f"{i:d}: {sys.argv[i]:}".format(i).format(sys.argv[i]))
-else:
-    print(f"{count:d} arguments:".format(count))
-    for i in range(1, len(sys.argv)):
-        print(f"{i:d}: {sys.argv[i]:}".format(i).format(sys.argv[i]))
+if __name__ == "__main__":
+    count = len((sys.argv)) - 1
+    if count == 0:
+        print(f"{count:d} arguments.".format(count))
+    elif count == 1:
+        print(f"{count:d} argument:".format(count))
+        for i in range(1, len(sys.argv)):
+            print(f"{i:d}: {sys.argv[i]:}".format(i).format(sys.argv[i]))
+    else:
+        print(f"{count:d} arguments:".format(count))
+        for i in range(1, len(sys.argv)):
+            print(f"{i:d}: {sys.argv[i]:}".format(i).format(sys.argv[i]))
