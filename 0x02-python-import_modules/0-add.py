9851ac5f419008d68956541533f6c96a2b2f4660
rahma korish <rahmakorish@gmail.com>
2023-11-30 07:32:10 -0800

    fix 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,5 +2,6 @@
 from add_0 import add
 a = 1
 b = 2
-x = add(a, b)
-print(f"{a:d} + {b:d} = {add(a, b):d}".format(a).format(b).format(add(a, b)))
+if __name__ == "__main__":
+    x = add(a, b)
+    print(f"{a:d} + {b:d} = {add(a, b):d}".format(a).format(b).format(add(a, b)))
