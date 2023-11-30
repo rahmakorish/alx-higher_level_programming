54b5606764155df3d4d78b68b28d53326a19e0d6
rahma korish <rahmakorish@gmail.com>
2023-11-30 07:55:45 -0800

    3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == "__main__":
     count = 0
     count = len((sys.argv)) - 1
-    for i in range (1, count):
+    for i in range(1, count):
         num = int(sys.argv[i])
         count += num
     print(f"{count:d}".format(count))
