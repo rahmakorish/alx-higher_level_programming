1031a3f0f127f7be169a5da47d0d36c0cc21fa5e
rahma korish <rahmakorish@gmail.com>
2023-11-30 07:18:31 -0800

    2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+count = len((sys.argv)) - 1
+if count == 0:
+    print(f"{count:d} arguments.".format(count))
+elif count == 1:
+    print(f"{count:d} argument:".format(count))
+    for i in range(1, len(sys.argv)):
+        print(f"{i:d}: {sys.argv[i]:}".format(i).format(sys.argv[i]))
+else:
+    print(f"{count:d} arguments:".format(count))
+    for i in range(1, len(sys.argv)):
+        print(f"{i:d}: {sys.argv[i]:}".format(i).format(sys.argv[i]))
