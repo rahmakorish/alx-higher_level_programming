16b1b6a4653d012eb6e91f4a16e66932cf8ee345
rahma korish <rahmakorish@gmail.com>
2023-11-30 07:37:58 -0800

    fix 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,8 @@
 from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-print(f"{a:d} + {b:d} = {add(a, b):d}".format(a).format(b).format(add(a, b)))
-print(f"{a:d} - {b:d} = {sub(a, b):d}".format(a).format(b).format(sub(a, b)))
-print(f"{a:d} * {b:d} = {mul(a, b):d}".format(a).format(b).format(mul(a, b)))
-print(f"{a:d} / {b:d} = {div(a, b):d}".format(a).format(b).format(div(a, b)))
+if __name__ == "__main__":
+    print(f"{a:d} + {b:d} = {add(a, b):d}".format(add(a, b)))
+    print(f"{a:d} - {b:d} = {sub(a, b):d}".format(sub(a, b)))
+    print(f"{a:d} * {b:d} = {mul(a, b):d}".format(mul(a, b)))
+    print(f"{a:d} / {b:d} = {div(a, b):d}".format(div(a, b)))
