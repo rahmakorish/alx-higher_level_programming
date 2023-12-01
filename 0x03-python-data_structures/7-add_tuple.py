8b84eb679d44c754431717a343b5c9cada173807
rahma korish <rahmakorish@gmail.com>
2023-12-01 13:48:12 -0800

    7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    for x in range(0, len(tuple_a)):
+        for i in range(0,len(tuple_b)):
+            print(tuple_a[x])
+            print(tuple_b[i])
