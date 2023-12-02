20ab5d69f1dde1d6f0f8d1e08187dc3b0cf7c3ee
rahma korish <rahmakorish@gmail.com>
2023-12-02 01:39:27 -0800

    2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif idx > len(my_list):
+        return my_list
+    else:
+        for x in range(0, len(my_list)):
+            if x == idx:
+                my_list[x] = element
+                return (my_list)
