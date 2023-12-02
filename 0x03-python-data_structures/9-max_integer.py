dd2edd3b061276a4b54d2cf954d55d39eb945526
rahma korish <rahmakorish@gmail.com>
2023-12-02 02:57:09 -0800

    9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max = my_list[0]
+    if len(my_list) == 0:
+        return (None)
+    else:
+        for x in range(0, len(my_list)):
+            if my_list[x] > max:
+                max = my_list[x]
+    return (max)
