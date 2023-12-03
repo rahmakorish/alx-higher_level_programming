5642481748e93948d3263d74103735a50911220e
rahma korish <rahmakorish@gmail.com>
2023-12-03 11:08:37 -0800

    6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return
     for raw in matrix:
+        if raw is None:
+            return
+        lenr = len(raw) - 1
         for x in range(0, len(raw)):
-            print("{:d}".format(raw[x]),end = '\n' if x == len(raw) - 1 else ' ')
+            print("{:d}".format(raw[x]), end='\n' if x == lenr else ' ')
