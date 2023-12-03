573a24e5b7940e0cb12ffc3d90ec2c75bfe1e1a0
rahma korish <rahmakorish@gmail.com>
2023-12-03 08:48:37 -0800

    7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,5 +5,11 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) >= 2 & len(tuple_b) >= 2:
         num1 = tuple_a[0] + tuple_b[0]
         num2 = tuple_a[1] + tuple_b[1]
+    elif 1 <= len(tuple_b) < 2:
+        num1 = tuple_b[0] + tuple_a[0]
+        num2 = 0 + tuple_a[1]
+    elif len(tuple_b) == 0:
+        num1 = 0 + tuple_a[0]
+        num2 = 0 + tuple_a[1]
     new = (num1, num2)
     return (new)
