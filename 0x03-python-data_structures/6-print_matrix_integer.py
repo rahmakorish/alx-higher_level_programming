7b9e7a59de2404a04e18769107c86607af954e13
rahma korish <rahmakorish@gmail.com>
2023-12-03 04:55:47 -0800

    extra

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for raw in(matrix):
+                print(raw)
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    for x in range(0, len(tuple_a)):
-        for i in range(0,len(tuple_b)):
-            print(tuple_a[x])
-            print(tuple_b[i])
+    for x in range(0, 2):
+            num1 = (tuple_a[x])
+            num2 = tuple_b[x]
+            return num1,num2
