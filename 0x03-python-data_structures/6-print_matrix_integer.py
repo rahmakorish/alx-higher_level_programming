3a2a5b3e6dcbf0fbf162937b486e530bd0061633
rahma korish <rahmakorish@gmail.com>
2023-12-03 11:03:52 -0800

    6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for raw in(matrix):
-                print(raw)
+    for raw in matrix:
+        for x in range(0, len(raw)):
+            print("{:d}".format(raw[x]),end = '\n' if x == len(raw) - 1 else ' ')
