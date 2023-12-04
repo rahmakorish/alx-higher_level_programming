98cd5a2ca81de476c566a57b0a940a25ba6cb151
rahma korish <rahmakorish@gmail.com>
2023-12-04 09:46:46 -0800

    13

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,2 +1,4 @@
+#include <stdio.h>
+#include <string.h>
+void print_python_list_info(PyObject *p):
 
-void print_python_list_info(PyObject *p);
---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ *is_palindrome-checks if a singly linked list is a palindrome
+ *@head:start of linked list
+ *Return:0 if palindrame
+**/
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL)
+	return 0;
+return(1);
+}
