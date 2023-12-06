df84baa3a8e2aebcd8df82037b3490666814d9e9
rahma korish <rahmakorish@gmail.com>
2023-12-06 06:09:27 -0800

    8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,8 +3,8 @@ def simple_delete(a_dictionary, key=""):
     if a_dictionary is None:
         return
     else:
+        new = a_dictionary.copy()
         for x in a_dictionary.keys():
-
             if x == key:
-                a_dictionary.pop(key)
-        return a_dictionary
+                new.pop(key)
+        return new
