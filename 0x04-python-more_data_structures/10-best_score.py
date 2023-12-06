f50278f1b99aaefda6551824b3ca90aaf4c56992
rahma korish <rahmakorish@gmail.com>
2023-12-06 05:53:51 -0800

    10 fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
-        return
+        return None
     else:
         big = 0
         for k, v in a_dictionary.items():
             if v > big:
                 big = v
                 first = k
-        return(first)
+        return first
