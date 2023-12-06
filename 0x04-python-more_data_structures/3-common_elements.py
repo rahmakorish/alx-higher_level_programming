a6307d956231d532d704758f2f547a80b01c2704
rahma korish <rahmakorish@gmail.com>
2023-12-06 05:29:34 -0800

    3 fix

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     if set_1 is None:
-        return
+        return None
     if set_2 is None:
-        return
+        return None
     else:
+        new = set()
         for x in set_1:
             for i in set_2:
                 if x == i:
-                    return x
+                    new.add(x)
+        return new
