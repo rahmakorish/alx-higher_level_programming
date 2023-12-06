c1a597f4e719c5ea23859cae4fe638e487b84fe9
rahma korish <rahmakorish@gmail.com>
2023-12-06 05:46:47 -0800

    9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     if a_dictionary is None:
-        return
+        return None
     else:
         new = {}
         for x, y in a_dictionary.items():
-            new[x] = y * 2
+            new.update({x: y * 2})
         return new
