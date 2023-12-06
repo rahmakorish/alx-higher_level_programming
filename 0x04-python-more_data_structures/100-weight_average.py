bd1a2effeee9506f82c032a6544f116b1cffdd64
rahma korish <rahmakorish@gmail.com>
2023-12-06 13:45:24 -0800

    100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-     for setx in my_list:
-         return type(setx)
+    if my_list is None:
+        return None
+    new = set()
+    low = 0
+    mul = 0
+    sumx = 0
+    for setx in my_list:
+        low += setx[1]
+        mul += (setx[0] * setx[1])
+    return mul / low
