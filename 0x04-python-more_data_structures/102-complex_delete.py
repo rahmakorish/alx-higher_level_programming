1849b619fad17c8cf0a4a3b28304c3df0d2ac789
rahma korish <rahmakorish@gmail.com>
2023-12-06 15:03:52 -0800

    extra

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,9 +2,9 @@
 def complex_delete(a_dictionary, value):
     if a_dictionary is None:
         return
-    else:
-        new = a_dictionary.copy()
-        for y, x in a_dictionary.items():
-            if x == value:
-                new.pop(y)
-        return new
+    new = a_dictionary.copy()
+    for y, x in a_dictionary.items():
+        if x == value:
+            new.pop(y)
+    a_dictionary = new.copy()
+    return a_dictionary
