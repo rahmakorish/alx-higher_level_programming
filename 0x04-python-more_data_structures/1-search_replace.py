8fa508d2b08d0224bb860f0e98fbddaac374aed0
rahma korish <rahmakorish@gmail.com>
2023-12-06 05:26:19 -0800

    1 fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_lis is None:
+    if my_list is None:
         return
     new = my_list.copy()
     for x in range(len(new)):
