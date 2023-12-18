91ba968ba00d57ba66ca1235d821580830e4b5bc
rahma korish <rahmakorish@gmail.com>
2023-12-18 13:55:08 -0800

    code style

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         while count < x:
             new.append(my_list[count])
             count += 1
-    except IndexError, TypeError, ValueError:
+    except IndexError:
         pass
     print(''.join(map(str, new)))
     return (count)
