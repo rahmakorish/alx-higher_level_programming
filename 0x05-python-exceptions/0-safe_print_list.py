613de7c02c7bd251c989ade0d6ee29156caf5715
rahma korish <rahmakorish@gmail.com>
2023-12-18 04:46:15 -0800

    extra

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         while count < x:
             new.append(my_list[count])
             count += 1
-    except(IndexError):
+    except(IndexError, TypeError):
         pass
     print(''.join(map(str, new)))
     return count
--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as msg:
+    except (ValueError, TypeError) as msg:
         print("Exception: {}".format(msg), file=sys.stderr)
         return False
