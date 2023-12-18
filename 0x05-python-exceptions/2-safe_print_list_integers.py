9736f22a3f9301ccd4727a3c8dba30a963384cc0
rahma korish <rahmakorish@gmail.com>
2023-12-18 02:12:37 -0800

    3

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
+    new = []
     try:
         for i in range (0, x):
-            print("{:d}".format(my_list[i]))
-            count += 1
+            if my_list[i].isdigit():
+                new.append(my_list[i])
+                count += 1
     except (TypeError, ValueError):
         pass
         #count -= 1
+    print("".join(map(str,new)))
     return (count)
