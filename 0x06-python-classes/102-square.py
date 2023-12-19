0e5f4c45950750ac153b7d20307b7d1d6cb80549
rahma korish <rahmakorish@gmail.com>
2023-12-19 09:35:11 -0800

    102

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -22,7 +22,8 @@ class Square:
     def area(self):
         sizex = self._size
         return (int(sizex * sizex))
+    """function to equal"""
     def __eq__(self, other):
         if isinstance(other, Square):
-            return self.area() == other.area()
+            return True
         return False
