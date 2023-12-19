70de6908a2f5cf678db0ddc6ecf4997a7bed6599
rahma korish <rahmakorish@gmail.com>
2023-12-19 01:17:17 -0800

    0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""python square module"""
+
+
 class Square:
     """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
     def __init__(self):
+        """constructor.
+        """
         pass
