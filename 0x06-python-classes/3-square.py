6ac1b2d94af68fe57702c228eff60def6fbcf213
rahma korish <rahmakorish@gmail.com>
2023-12-19 01:28:36 -0800

    3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+"""python file"""
+
+
 class Square:
     """a class Square that defines a square"""
     def __init__(self, size=0):
-        self.size = size
+        """constructor.
+        Args:
+        size:square size"""
+        self._size = size
         if not type(size) is int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
     """function to find square area"""
     def area(self):
-        return (self.size * self.size)
+        return (self._size * self._size)
