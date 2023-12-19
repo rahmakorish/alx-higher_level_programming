57a61b6898b4412086d5e11707676ec98595849d
rahma korish <rahmakorish@gmail.com>
2023-12-19 01:20:02 -0800

    1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""python file"""
+
+
 class Square:
+    """squar class"""
     def __init__(self, size):
+        """constructor.
+        Args:
+            size:square size
+        """
         self._size = size
