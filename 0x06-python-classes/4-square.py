3b3416de293c908489c4d328ed11c263d7c7ed01
rahma korish <rahmakorish@gmail.com>
2023-12-19 02:19:53 -0800

    4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,16 +7,18 @@ class Square:
     def __init__(self, size=0):
         self._size = size
     """getter function for size"""
+    @property
     def size(self):
         return (self._size)
     """setter function for size"""
+    @size.setter
     def size(self, value):
         self._size = value
-        if not type(size) is int:
+        if not type(self._size) is int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if self._size < 0:
             raise ValueError("size must be >= 0")
     """function to return squarea area"""
     def area(self):
-        sizex =self._size
+        sizex = self._size
         return (sizex * sizex)
