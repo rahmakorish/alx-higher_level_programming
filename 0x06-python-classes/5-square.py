3f5f770c068f31a5b518a4779d7aefbb3df1179d
rahma korish <rahmakorish@gmail.com>
2023-12-19 02:36:43 -0800

    5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""python file"""
+
+class Square:
+    """class Square that defines a square"""
+    def __init__(self, size=0):
+        self._size = size
+    """getter function for size"""
+    @property
+    def size(self):
+        return (self._size)
+    """setter function for size"""
+    @size.setter
+    def size(self, value):
+        self._size = value
+        if not type(self._size) is int:
+            raise TypeError("size must be an integer")
+        if self._size < 0:
+            raise ValueError("size must be >= 0")
+    """function to return squarea area"""
+    def area(self):
+        sizex = self._size
+        return (sizex * sizex)
+    def my_print(self):
+        sqr = self.area()
+        if self._size == 0:
+            print("")
+        else:
+            for x in range(0, self._size+1):
+                if x == self._size:
+                    print(end="\n")
+                    x = 0
+                else:
+                    print(x * "#", end='')
+
