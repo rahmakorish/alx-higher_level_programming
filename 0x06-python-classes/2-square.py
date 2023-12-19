41f0e19f1d15731c8db42cbd7667579b896e10be
rahma korish <rahmakorish@gmail.com>
2023-12-19 01:24:11 -0800

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""pyrhon file """
+
+
 class Square:
     """a class Square that defines a square"""
     def __init__(self, size=0):
+        """constructor.
+        Args:
+            size:square size
+            """
         self.size = size
         if not type(size) is int:
             raise TypeError("size must be an integer")
