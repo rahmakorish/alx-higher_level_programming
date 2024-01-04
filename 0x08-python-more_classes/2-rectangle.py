d90bf11cab81d1a7cc1e560dfe843b5fe347ae1b
rahma korish <rahmakorish@gmail.com>
2024-01-04 10:27:38 -0800

    perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -8,36 +8,42 @@ class Rectangle:
         """constructor."""
         self.width = width
         self.height = height
-    """getter function for width"""
+
     @property
     def width(self):
+        """getter function for width"""
         return (self.__width)
-    """setter for width"""
+
     @width.setter
     def width(self, value):
+        """setter for width"""
         if not type(value) is int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    """getter function for height"""
+
     @property
     def height(self):
+        """getter function for height"""
         return (self.__height)
-    """setter for heigth"""
+
     @height.setter
     def height(self, value):
+        """setter for heigth"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-    """function to return squarea area"""
+
     def area(self):
+        """function to return squarea area"""
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width * self.__height)
-    """function to return rectangle perimeter"""
+
     def perimeter(self):
+        """function to return rectangle perimeter"""
         perimet = (self.__width + self.__height) * 2
         return (perimet)
