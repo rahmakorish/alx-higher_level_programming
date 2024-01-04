6f6ba5b392e4bc2603ab9bd1ad18f18c1f361473
rahma korish <rahmakorish@gmail.com>
2024-01-04 08:18:48 -0800

    class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,26 +8,30 @@ class Rectangle:
         """constructor."""
         self.__width = width
         self.__height = height
-    """getter function for width"""
+    # """getter function for width"""
     @property
     def width(self):
+        """getter function for width"""
         return (self.__width)
-    """setter for width"""
+    # """setter for width"""
     @width.setter
     def width(self, value):
+        """setter for width"""
         # self.__width = value
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    """getter function for height"""
+   # """getter function for height"""
     @property
     def height(self):
-        return (self.__height)
     """setter for heigth"""
+        return (self.__height)
+   # """setter for heigth"""
     @height.setter
     def height(self, value):
+        """setter for heigth"""
         # self.__height = value
         if type(value) is not int:
             raise TypeError("height must be an integer")
