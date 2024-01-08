95c55d97db0aa37384f31f875862d829b959e51d
rahma korish <rahmakorish@gmail.com>
2024-01-08 00:41:03 -0800

    2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class inheretence"""
+
+def is_same_class(obj, a_class):
+    """class"""
+    x = isinstance(obj, a_class)
+    if x :
+        return True
+    else:
+        return False
