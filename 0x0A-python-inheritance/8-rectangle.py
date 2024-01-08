2a36bdebca4d6a505d852f4b25808a00b6064fb9
rahma korish <rahmakorish@gmail.com>
2024-01-08 03:49:06 -0800

    8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""base class"""
+
+
+class BaseGeometry:
+    """class"""
+    def __init__(self):
+        """class"""
+    def area(self):
+        """class method"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """class method"""
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """class"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = (height)
+
