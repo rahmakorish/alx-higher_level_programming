371b2c36ef5a4f86c44eb3501f2ebe1418dab6b0
rahma korish <rahmakorish@gmail.com>
2024-01-08 03:38:13 -0800

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""base class"""
+
+
+class BaseGeometry:
+    """class"""
+    def __init__(self):
+        """class"""
+    def area(self):
+        raise Exception("area() is not implemented")
