2ab296a6e295f7a0e574bd84fec1dcfbb886cd28
rahma korish <rahmakorish@gmail.com>
2024-01-08 00:58:28 -0800

    100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class"""
+
+class MyInt(int):
+    """class rebel"""
+    def __init__(self):
+        """intial"""
+
+        return (self)
+    """
+    def arthmatic 
+        self.equal = "!="
+        self.notequal = "=="""
