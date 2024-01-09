32226479cbd74bac4239e2096d97a9ac158d1782
rahma korish <rahmakorish@gmail.com>
2024-01-09 07:26:40 -0800

    advanced2

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,6 +4,10 @@
 
 class MyInt(int):
     """class rebel"""
-    def arthmatic(self, num):
+    def __arthmatic__(self, num):
         """over ride function"""
         return not super().__arthmatic__(num)
+
+    def __equal__(self, num):
+        """over ride function"""
+        return not super().__equal__(num)
