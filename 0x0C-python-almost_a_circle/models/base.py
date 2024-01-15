987ab02314cdd909f33b9244865dd5d260e42c01
rahma korish <rahmakorish@gmail.com>
2024-01-15 11:57:53 -0800

    json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """returns the JSON string"""
-        if list_dictionaries is None or list_dictionaries == '':
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return ('[]')
         return (json.dumps(list_dictionaries))
 
