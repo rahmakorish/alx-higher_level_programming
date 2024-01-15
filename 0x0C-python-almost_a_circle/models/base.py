1c5e58ec8d21c5c423dd5203ad87e4c74bea4d15
rahma korish <rahmakorish@gmail.com>
2024-01-15 13:40:14 -0800

    json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,6 +16,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
@@ -23,6 +24,7 @@ class Base:
         else:
             return (json.dumps(list_dictionaries))
 
+    @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON string"""
         if json_string is None or len(json_string) == 0:
