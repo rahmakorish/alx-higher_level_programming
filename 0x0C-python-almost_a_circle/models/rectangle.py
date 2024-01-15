b3ca1f5dcbec7dd0972909b804fb4dd2931812ed
rahma korish <rahmakorish@gmail.com>
2024-01-15 10:21:15 -0800

    f

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,8 +81,7 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """print dictionary"""
-        return {'id': self.id, 'width': self.width\,
-                'height': self.height, 'x': self.x, 'y': self.y}
+        return {'id': self.id, 'width': self.width, 'height': self.height, 'x': self.x, 'y': self.y}
 
     def display(self):
         """override display()"""
