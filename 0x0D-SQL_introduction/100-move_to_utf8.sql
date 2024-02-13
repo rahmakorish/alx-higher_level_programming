89bb3ed5d2508b081a6ca302cc29e043bf994829
rahma korish <rahmakorish@gmail.com>
2024-02-13 11:48:05 -0800

    extra

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- script that converts hbtn_0c_0 database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_bin;
+SET foreign_key_checks = 0;
+-- ALTER TABLE first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_bin;
