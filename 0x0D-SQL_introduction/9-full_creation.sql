bf72735256426f5d3cc00b0743414a1d2f5ecaa2
rahma korish <rahmakorish@gmail.com>
2024-02-13 10:58:16 -0800

    FULL CREATE

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,9 +5,10 @@ CREATE TABLE IF NOT EXISTS second_table(
 	score INT
 );
 INSERT INTO second_table (id, name, score)
-VALUES
-(1, "John", 10),
-(2, "ALEX", 3),
-(3, "BOB", 14),
-(4, "GEORGE", 8);
-
+VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score)
+VALUES (2, "ALEX", 3);
+INSERT INTO second_table (id, name, score)
+VALUES (3, "BOB", 14);
+INSERT INTO second_table (id, name, score)
+VALUES(4, "GEORGE", 8);
