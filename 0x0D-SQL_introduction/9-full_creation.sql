bf342bc4f2232b6c7f3d719c0cb7eb4e9b34493c
rahma korish <rahmakorish@gmail.com>
2024-02-13 08:10:48 -0800

    second table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+--  a script that creates a table second_table
+CREATE TABLE second_table(
+	id int,
+	name VARCHAR(256),
+	score int
+);
+INSERT INTO second_table (id, name, score)
+VALUES
+(1, "John", 10),
+(2, "ALEX", 3),
+(3, "BOB", 14),
+(4, "GEORGE", 8);
