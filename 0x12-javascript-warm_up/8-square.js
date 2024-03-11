8e5a36d44a189d8410b58034128e39704172ac1f
rahma <rahmakorish@gmail.com>
2024-03-11 12:25:39 -0700

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,14 +2,23 @@
 const i = process.argv[2];
 let x = 0;
 let j = 0;
+let out = '';
+let total = '';
 if (!Number(i)) {
   console.log('Missing size');
 } else {
   while (j < i) {
     while (x < i) {
-      console.log('X');
+      out += 'X';
       x++;
     }
+    total += out;
     j++;
+    if (j !== i) {
+      total += '\n';
+    } else {
+      continue;
+    }
   }
 }
+console.log(total);
