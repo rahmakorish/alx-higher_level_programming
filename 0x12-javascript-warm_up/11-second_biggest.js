10f60099d5dc9cbc543d904b1e1758a162f446fe
rahma <rahmakorish@gmail.com>
2024-03-11 12:07:34 -0700

    2nd best

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.length === 2) {
+    console.log(0);
+  } else if (process.argv.length === 3) {
+    console.log(0);
+  } else {
+    console.log(process.argv[3]);
+  }
