70a881c33f55ef010ed138a3617704f7e5b0d484
rahma <rahmakorish@gmail.com>
2024-03-11 04:35:39 -0700

    argcs

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (process.argv == 1) {
-console.log('No argument');
-} else if (process.argv > 1) {
-console.log('Argument found');
+if (process.argv === 1) {
+  console.log('No argument');
+} else {
+  console.log('Argument found');
 }
