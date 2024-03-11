c47c2be788d7626b0a6be7efe96534b6b0b61b92
rahma <rahmakorish@gmail.com>
2024-03-11 13:25:48 -0700

    last factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,14 +3,14 @@ const inputy = Number(process.argv[2]);
 function factorialnum (x) {
   let sum = 1;
   let f = x;
-  if (f === 1){
-  sum = NaN;
+  if (f === 1) {
+    sum = NaN;
   } else {
-  while (f > 1) {
-    sum *= f;
-    f--;
+    while (f > 1) {
+      sum *= f;
+      f--;
+    }
   }
-}
   console.log(sum);
 }
 factorialnum(inputy);
