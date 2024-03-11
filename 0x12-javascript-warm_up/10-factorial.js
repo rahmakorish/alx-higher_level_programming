8e3cba48fea95cb9b885609e550b6bc368c825ed
rahma <rahmakorish@gmail.com>
2024-03-11 13:21:51 -0700

    last factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-const x = Number(process.argv[2]);
-let sum = 1;
-let f = x;
-while (f > 1) {
-  sum *= f;
-  f--;
+const inputy = Number(process.argv[2]);
+function factorialnum (x) {
+  let sum = 1;
+  let f = x;
+  while (f > 1) {
+    sum *= f;
+    f--;
+  }
+  console.log(sum);
 }
-console.log(sum);
+factorialnum(inputy);
