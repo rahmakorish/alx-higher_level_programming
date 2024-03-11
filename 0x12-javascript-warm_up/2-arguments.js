9c65143b2e0e24e8da298eee1e9d5a2393a1798e
rahma <rahmakorish@gmail.com>
2024-03-11 04:46:24 -0700

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-if (arguments.length === 1) {
-  console.log('No argument');
-} else if (arguments.length === 2) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
+console.log(arguments.length);
