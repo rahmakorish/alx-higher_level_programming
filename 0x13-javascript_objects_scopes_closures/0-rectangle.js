b9117930beed59f240bcb317ba84f03e16792dfa
rahma <rahmakorish@gmail.com>
2024-03-12 12:33:12 -0700

    first class

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-exports.Rectangle = class Rectangle {
-	constructor(width, height) {
-    this.width = width;
-    this.height = height;
-  }
-};
+class Rectangle {
+
+}
+module.exports = Rectangle;
