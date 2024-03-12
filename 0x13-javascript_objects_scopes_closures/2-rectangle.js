3d17f00885ff44c6912594c1f9bb93859e5aaa76
rahma <rahmakorish@gmail.com>
2024-03-12 13:05:01 -0700

    empty obj

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (h <= 0 || w <= 0) {
-      return{};
-    } else {
-    this.width = w;
-    this.height = h;
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-
 }
 module.exports = Rectangle;
