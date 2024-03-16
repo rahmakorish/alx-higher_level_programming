f8793a5f376bc461d0a9bbecfd3ff824c2ad8457
rahma <rahmakorish@gmail.com>
2024-03-16 15:02:08 -0700

    last 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,6 +10,8 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
+        host="localhost",
+        port=3306
     )
     value=sys.argv[4]
     cur = mydb.cursor()
