4aad5a43bce380546a6fd5a3b3d0c452dd734244
rahma <rahmakorish@gmail.com>
2024-03-16 16:14:01 -0700

    all

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,10 +7,10 @@ import MySQLdb
 
 if __name__ == "__main__":
     mydb = MySQLdb.connect(
+        host="localhost",
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
-        host="localhost",
         port=3306
     )
     cur = mydb.cursor()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,10 +8,10 @@ import MySQLdb
 if __name__ == "__main__":
     mydb = MySQLdb.connect(
         host="localhost",
-        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
+        port=3306
     )
     cur = mydb.cursor()
     cur.execute("SELECT * FROM cities ORDER BY cities.id ASC;")
