63819e5dfa0a0561494894eddb62794f980da9d1
rahma <rahmakorish@gmail.com>
2024-03-17 13:57:23 -0700

    8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""list all states"""
-
-
+"""
+List all states
+"""
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy import sessionmaker
-from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import sessionmaker
 import sys
+
+
 if __name__ == '__main__':
-    db = "mysql+pymysql://{}:{}@localhost:3306/{}".format(sys.argv[1], sys.argv[2], sys.argv[3])
-    engine = create_engine(db, pool_pre_ping=True)
+    engine = create_engine("mysql+pymysql://{}:{}@localhost:3306/{}".
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State).order_by(State.id).last()
+
     if states:
-        print('{}: {}'.format(state.id, state.name))
+        print('{}: {}'.format(states.id, states.name))
     else:
         print('Nothing')
