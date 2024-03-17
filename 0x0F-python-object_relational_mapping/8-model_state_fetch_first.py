b4b6064d688f434b2debe33c8fba0cef1354d5b9
rahma <rahmakorish@gmail.com>
2024-03-17 13:48:41 -0700

    modify 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -4,22 +4,16 @@
 
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy import MetaData
-from sqlalchemy import Table
+from sqlalchemy import sessionmaker
 from sqlalchemy.engine.url import URL
 import sys
 if __name__ == '__main__':
-    db = {
-        'username': sys.argv[1],
-        'password': sys.argv[2],
-        'drivername': sys.argv[3],
-        'host': 'localhost',
-        'port': 3306
-    }
-    engine = create_engine('sqlite:///7-model_state_fetch_all.sql')
-
-    result = engine.execute('SELECT id: name FROM states WHERE id = 1 ORDER BY states.id;')
-    if result:
-        print(result)
+    db = "mysql+pymysql://{}:{}@localhost:3306/{}".format(sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(db, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).last()
+    if states:
+        print('{}: {}'.format(state.id, state.name))
     else:
         print('Nothing')
