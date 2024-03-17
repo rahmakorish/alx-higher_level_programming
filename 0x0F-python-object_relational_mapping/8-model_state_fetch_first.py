efa4c128ccb56de5985b5ec90d567b17d3a24b96
rahma <rahmakorish@gmail.com>
2024-03-17 08:12:25 -0700

    8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""list all states"""
+
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy import MetaData
+from sqlalchemy import Table
+from sqlalchemy.engine.url import URL
+import sys
+if __name__ == '__main__':
+    db = {
+        'username': sys.argv[1],
+        'password': sys.argv[2],
+        'drivername': sys.argv[3],
+        'host': 'localhost',
+        'port': 3306
+    }
+    engine = create_engine('sqlite:///7-model_state_fetch_all.sql')
+
+    result = engine.execute('SELECT id: name FROM states WHERE id = 1 ORDER BY states.id;')
+    for state in result:
+        print(state)
