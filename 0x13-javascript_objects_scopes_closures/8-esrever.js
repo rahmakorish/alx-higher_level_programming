0443a08191ff9e0eb04b9d73a1e99e52d5d6a22f
rahmakorish <84594899+rahmakorish@users.noreply.github.com>
2024-06-06 19:55:26 +0300

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-exports.esrever = function (list) {
-  const newlist = [];
-  for (let i = list.length - 1, j = 0; i >= 0, j < list.length; i--, j++) {
-    newlist[j] = list[i];
-  }
-  return newlist;
-};
