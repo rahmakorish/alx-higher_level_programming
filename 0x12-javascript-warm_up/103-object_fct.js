b5203c643fb82f5b268da6124f7713cb4f42cb9b
rahma <rahmakorish@gmail.com>
2024-06-19 01:23:25 +0300

    last

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-function incr(){
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+function incr () {
   self.value++;
 }
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
