08aea17e490a831dd17de2c432ec3462063c2012
rahma <rahmakorish@gmail.com>
2024-06-23 15:15:56 +0300

    first

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""python file"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    my_connection = MySQLdb.connect(
+        host="localhost",
+        db=sys.argv[3],
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        port=3306
+    )
+
+    mycursor = my_connection.cursor()
+    mycursor.execute("SELECT * FROM states ORDER BY id ASC;")
+    states = mycursor.fetchall()
+    for state in states:
+        print(state)
+    mycursor.close()
+    my_connection.close()
---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""python file"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    my_connection = MySQLdb.connect(
+        host="localhost",
+        db=sys.argv[3],
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        port=3306
+    )
+
+    cur = my_connection.cursor()
+    cur.execute("""SELECT * FROM states WHERE name LIKE \
+                'N%' ORDER BY states.id""")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    my_connection.close()
