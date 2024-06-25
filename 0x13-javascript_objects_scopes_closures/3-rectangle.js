4401be63ef690b0f0e7fb5e3b5582b49d55f4da2
rahma <rahmakorish@gmail.com>
2024-06-25 20:10:52 +0300

    3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-    print(){
-        for(let i = 0; i < w; i++)
-            {
-                for(let x = 0; x < h; x++)
-                    console.log('#');
-            }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-  module.exports = Rectangle;
-  
+
+  print () {
+    for (let i = 0; i < this.w; i++) {
+      for (let x = 0; x < this.h; x++) {
+        console.log('#');
+      }
+    }
+  }
+}
+module.exports = Rectangle;
